93a8414bc228fe37c2ee77a34210c52f2371d700
Arkadii Humennyi <humennyi.arkadii@gmail.com>
2018-10-20 15:20:10 +0300

    minor fix

--- app/src/main/java/me/arkadzi/imho/domain/model/User.kt
+++ app/src/main/java/me/arkadzi/imho/domain/model/User.kt
@@ -2,7 +2,6 @@ package me.arkadzi.imho.domain.model
 
 open class User(val id: Long = 0,
                 val email: String,
-                val password: String,
                 val firstName: String,
                 val middleName: String,
                 val lastName: String,
