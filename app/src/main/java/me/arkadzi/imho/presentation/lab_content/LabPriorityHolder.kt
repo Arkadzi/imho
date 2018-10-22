cc194a63bd288796dcd36cb6e28a3ea46a391ec0
Arkadii Humennyi <humennyi.arkadii@gmail.com>
2018-10-22 19:51:41 +0300

    improve design

--- app/src/main/java/me/arkadzi/imho/presentation/lab_content/LabPriorityHolder.kt
+++ app/src/main/java/me/arkadzi/imho/presentation/lab_content/LabPriorityHolder.kt
@@ -3,9 +3,8 @@ package me.arkadzi.imho.presentation.lab_content
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import kotlinx.android.extensions.LayoutContainer
-import kotlinx.android.synthetic.main.item_diploma.*
+import kotlinx.android.synthetic.main.item_lab.*
 import me.arkadzi.imho.R
-import me.arkadzi.imho.domain.model.Lab
 import me.arkadzi.imho.domain.model.LabPriority
 import me.arkadzi.imho.presentation.adapters.BaseHolder
 
@@ -16,6 +15,7 @@ class LabPriorityHolder(
         LayoutContainer {
 
     override fun bind(data: LabPriority) {
+        ivIcon.setImageResource(R.drawable.ic_priority)
         tvTitle.text = data.title
     }
 }
